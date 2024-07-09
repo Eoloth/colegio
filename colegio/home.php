e6ec3e72b50be3e0be699e49078ab15970712978
Ruben <ruben.msalles@gmail.com>
2024-07-09 11:31:26 -0400

    📝 (about.html): actualizar enlaces de navegación en la barra de navegación 📝 (contact.html): actualizar enlaces de navegación en la barra de navegación 📝 (eventos.html): actualizar enlaces de navegación en la barra de navegación 📝 (galeria.html): actualizar enlaces de navegación en la barra de navegación
    ♻️ (home.php): refactorizar la ruta del enlace "Inicio" para que apunte a "home.php" en lugar de "index.html"

--- colegio/home.php
+++ colegio/home.php
@@ -82,29 +82,29 @@ if (isset($_SESSION['mensaje'])) {
 
 <!-- Start header -->
 <header class="top-navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.html">
-                <img src="images/logo.png" alt="" />
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbars-host">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item active"><a class="nav-link" href="index.html">Inicio</a></li>
-                    <li class="nav-item"><a class="nav-link" href="about.html">Acerca de nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="eventos.html">Eventos</a></li>
-                    <li class="nav-item"><a class="nav-link" href="galeria.html">Galería de Imágenes</a></li>
-                    <li class="nav-item"><a class="nav-link" href="contact.html">Contacto</a></li>
-                    <li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#login">Entrar</a></li>
-                </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.html">
+                    <img src="images/logo.png" alt="" />
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbars-host" aria-controls="navbars-rs-food" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbars-host">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active"><a class="nav-link" href="home.php">Inicio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="about.html">Acerca de nosotros</a></li>
+                        <li class="nav-item"><a class="nav-link" href="eventos.html">Eventos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="galeria.html">Galería de Imágenes</a></li>
+                        <li class="nav-item"><a class="nav-link" href="contact.html">Contacto</a></li>
+                        <li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#login">Entrar</a></li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-</header>
+        </nav>
+    </header>
 
 <div class="container">
     <?php if (isset($_SESSION['usuario'])): ?>
