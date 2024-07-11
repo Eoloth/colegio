c2844fbb715ce7680c815078e4e3f538921fb411
Ruben <ruben.msalles@gmail.com>
2024-07-10 20:33:52 -0400

    📝 (about.html): actualizar enlaces de eventos.html a eventos.php 📝 (galeria.php): actualizar enlaces de eventos.html a eventos.php

--- colegio/galeria.php
+++ colegio/galeria.php
@@ -113,7 +113,7 @@ if (isset($_SESSION['mensaje'])) {
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item"><a class="nav-link" href="home.php">Inicio</a></li>
                     <li class="nav-item"><a class="nav-link" href="about.html">Acerca de nosotros</a></li>
-                    <li class="nav-item"><a class="nav-link" href="eventos.html">Eventos</a></li>
+                    <li class="nav-item"><a class="nav-link" href="eventos.php">Eventos</a></li>
                     <li class="nav-item active"><a class="nav-link" href="galeria.php">Galería de Imágenes</a></li>
                     <li class="nav-item"><a class="nav-link" href="contact.html">Contacto</a></li>
                     <li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#login">Entrar</a></li>
@@ -187,7 +187,7 @@ if (isset($_SESSION['mensaje'])) {
                     <ul class="footer-links">
                         <li class="nav-item active"><a class="nav-link" href="home.php">Inicio</a></li>
                         <li class="nav-item"><a class="nav-link" href="about.html">Acerca de nosotros</a></li>
-                        <li class="nav-item"><a class="nav-link" href="eventos.html">Eventos</a></li>
+                        <li class="nav-item"><a class="nav-link" href="eventos.php">Eventos</a></li>
                         <li class="nav-item"><a class="nav-link" href="galeria.php">Galería de Imágenes</a></li>
                         <li class="nav-item"><a class="nav-link" href="contact.html">Contacto</a></li>
                         <li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#login">Entrar</a></li>
