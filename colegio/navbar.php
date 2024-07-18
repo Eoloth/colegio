26e591ae5ca5b5a44c46f3ca2906f6e43af88d4a
Ruben <ruben.msalles@gmail.com>
2024-07-18 14:54:11 -0400

    📝 (navbar.php): añadir modal de login al navbar para permitir a los usuarios iniciar sesión 📝 (list_events.php): añadir dependencias de Bootstrap JS para el funcionamiento correcto de la página

--- colegio/navbar.php
+++ colegio/navbar.php
@@ -23,10 +23,45 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     <?php if (isset($_SESSION['usuario'])): ?>
                         <li class="nav-item"><a class="nav-link" href="uploads/logout.php">Cerrar Sesión</a></li>
                     <?php else: ?>
-                        <li class="nav-item"><a class="nav-link" href="" data-toggle="modal" data-target="#login">Entrar</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" data-toggle="modal" data-target="#login">Entrar</a></li>
                     <?php endif; ?>
                 </ul>
             </div>
         </div>
     </nav>
 </header>
+
+<!-- Modal de login -->
+<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body customer-box">
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <div class="tab-pane active" id="Login">
+                        <form role="form" class="form-horizontal" action="uploads/login.php" method="POST">
+                            <div class="form-group">
+                                <div class="col-sm-12">
+                                    <input class="form-control" id="usuario" name="usuario" placeholder="Usuario" type="text" required>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-12">
+                                    <input class="form-control" id="contraseña" name="contraseña" placeholder="Contraseña" type="password" required>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-10">
+                                    <button type="submit" class="btn btn-light btn-radius btn-brd grd1">
+                                        Entrar
+                                    </button>
+                                    <a class="for-pwd" href="forgot_password.php">¿Olvidaste tu contraseña?</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- colegio/uploads/list_events.php
+++ colegio/uploads/list_events.php
@@ -20,7 +20,21 @@ try {
 }
 ?>
 
-<?php include '../header.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Eventos</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="../css/versions.css">
+    <link rel="stylesheet" href="../css/responsive.css">
+    <link rel="stylesheet" href="../css/custom.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="../js/modernizer.js"></script>
+</head>
+<body class="host_version">
 <?php include '../navbar.php'; ?>
 
 <div class="container">
@@ -61,3 +75,9 @@ try {
 </div>
 
 <?php include '../footer.php'; ?>
+
+<!-- Incluir dependencias de Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
